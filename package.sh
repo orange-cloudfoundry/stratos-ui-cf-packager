551852eaa544c53141397661d926db82f1031b19
ArthurHlt <arthur.halet@orange.com>
2017-10-31 16:49:21 +0100

    fix ci

--- package.sh
+++ package.sh
@@ -13,6 +13,6 @@ fi
 mkdir cache
 mkdir build
 
-NODE_HOME="/home/travis/.nvm/versions/node/v7.4.0/bin" stratos-ui/deploy/cloud-foundry/build.sh ./build ./cache
+NODE_HOME="$(ls -d /home/travis/.nvm/versions/node/* | head -n 1)" stratos-ui/deploy/cloud-foundry/build.sh ./build ./cache
 
 zip -r stratos-ui-packaged ./build/*
