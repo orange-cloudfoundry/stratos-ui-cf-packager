922a7649d995cd0a62fe1fccfde4c1e89453015a
ArthurHlt <arthur.halet@orange.com>
2017-10-31 17:25:55 +0100

    fix ci

--- package.sh
+++ package.sh
@@ -11,10 +11,19 @@ if [ "x$TRAVIS_TAG" != "x" ]; then
 fi
 
 mkdir cache
-mkdir build
 CWD="$(pwd)"
 npm_lcation="$(which npm)"
-NODE_HOME="${npm_lcation%%/bin/npm}" stratos-ui/deploy/cloud-foundry/build.sh "$CWD/build" "$CWD/cache"
+NODE_HOME="${npm_lcation%%/bin/npm}" stratos-ui/deploy/cloud-foundry/build.sh "$CWD/stratos-ui" "$CWD/cache"
+
+# Remove the node_modules and bower_components folders - only needed for build
+if [ -d "$BUILD_DIR/node_modules" ]; then
+  rm -rf $BUILD_DIR/node_modules
+fi
+
+if [ -d "$BUILD_DIR/bower_components" ]; then
+  rm -rf $BUILD_DIR/bower_components
+fi
 
 cd "$CWD"
-zip -r stratos-ui-packaged ./build/*
+ls -lah ./stratos-ui
+zip -r stratos-ui-packaged ./stratos-ui/*
